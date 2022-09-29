0a730f516666254f22cb243aef9998bcdacfe1c9
MrKraack <MrKraack@gmail.com>
2022-09-29 11:36:55 +0200

    Hangman done

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,9 +1,16 @@
+//Get scoreboard from DOM
+const scoreboard = document.getElementById("scoreboard");
+//Get win loss text field from DOM
+let winLossText = document.getElementById("winLossText");
+
+window.onload = (event) => {
+    scoreboard.innerText = `Guess ${attempt} out of ${maxAttempt}`;
+  };
 // Array of the answers
 const hangmanAnswers = [
     "types",
     "operators",
     "functions",
-    "DOM",
     "events",
     "conditions",
     "loops",
@@ -11,154 +18,124 @@ const hangmanAnswers = [
     "arrays",
     "debugging",
     "cookies"
-    ]
+]
 
-    let answer = '';
-    let maxAttempt = 8;
-    let attempt = 0;
-    let guesses = [];
-    // let displayWord = null;
+let answer = '';
+let maxAttempt;
+let attempt = 0;
+let guesses = [];
+let displayWord = [];
 
-    // Takes random word out of array with answers
-    function randomWord() {
-        answer = hangmanAnswers [Math.floor(Math.random() * hangmanAnswers.length)];
-        
-    }
-    
-    
-    let keyboard = document.getElementById("keyboard");
-    
-    // Array for the keyboard
-    let dataSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
-    
-    // Takes the strings in the array and makes them to buttons
-    for (let i = 0; i < dataSet.length; i++) {
-        let alphabetBtn = document.createElement("button");
-        alphabetBtn.innerHTML = dataSet[i];
-        keyboard.appendChild(alphabetBtn);
-        alphabetBtn.setAttribute("id",i);
-        alphabetBtn.addEventListener("click", displayLetter);
-        
-        
-    }
-
-    let newA = document.createElement('p');
-    let newB = document.createElement('p');
-    let newC = document.createElement('p');
-    let newD = document.createElement('p');
-    let newE = document.createElement('p');
-    let newF = document.createElement('p');
-    let newG = document.createElement('p');
-    let newH = document.createElement('p');
-    let newI = document.createElement('p');
-    let newJ = document.createElement('p');
-    let newK = document.createElement('p');
-    let newL = document.createElement('p');
-    let newM = document.createElement('p');
-    let newN = document.createElement('p');
-    let newO = document.createElement('p');
-    let newP = document.createElement('p');
-    let newQ = document.createElement('p');
-    let newR = document.createElement('p');
-    let newS = document.createElement('p');
-    let newT = document.createElement('p');
-    let newU = document.createElement('p');
-    let newV = document.createElement('p');
-    let newX = document.createElement('p');
-    let newY = document.createElement('p');
-    let newZ = document.createElement('p');
-
-    newA.innerHTML = "_"
-    newB.innerHTML = "_"
-    newC.innerHTML = "_"
-    newD.innerHTML = "_"
-    newE.innerHTML = "_"
-    newF.innerHTML = "_"
-    newG.innerHTML = "_"
-    newH.innerHTML = "_"
-    newI.innerHTML = "_"
-    newJ.innerHTML = "_"
-    newK.innerHTML = "_"
-    newL.innerHTML = "_"
-    newM.innerHTML = "_"
-    newN.innerHTML = "_"
-    newO.innerHTML = "_"
-    newP.innerHTML = "_"
-    newQ.innerHTML = "_"
-    newR.innerHTML = "_"
-    newS.innerHTML = "_"
-    newT.innerHTML = "_"
-    newU.innerHTML = "_"
-    newV.innerHTML = "_"
-    newX.innerHTML = "_"
-    newY.innerHTML = "_"
-    newZ.innerHTML = "_"
-
-    newA.append("sectionP");
-    newB.append("sectionP");
-    newC.append("sectionP");
-    newD.append("sectionP");
-    newE.append("sectionP");
-    newF.append("sectionP");
-    newG.append("sectionP");
-    newH.append("sectionP");
-    newI.append("sectionP");
-    newJ.append("sectionP");
-    newK.append("sectionP");
-    newL.append("sectionP");
-    newM.append("sectionP");
-    newN.append("sectionP");
-    newO.append("sectionP");
-    newP.append("sectionP");
-    newQ.append("sectionP");
-    newR.append("sectionP");
-    newS.append("sectionP");
-    newT.append("sectionP");
-    newU.append("sectionP");
-    newV.append("sectionP");
-    newX.append("sectionP");
-    newY.append("sectionP");
-    newZ.append("sectionP");
-    
-
-    
-
-    
-    function displayLetter(choseLetter) {
-        
-        // guesses.indexOf(choseLetter) === -1 ? guesses.push(choseLetter) : null;
-        // document.getElementById(choseLetter).setAttribute('disabled', true);
-        
-        // if (answer.indexOf(choseLetter) >= 0) {
-        //     guessBox();
-        // }
-        // ^ FEJL på setAttibute (kan ikke finde idet fra bogstaverne)
-        
-    }
-    // displayLetter("O");
-    
-    
-    // 
-    function guessBox() {
-        displayWord = answer.split('').map(letters => (guesses.indexOf(letters) >= 0 ? letters : " _ ")).join('');
-        document.getElementById("sectionP").innerHTML = displayWord;
-        
-    }
-    
-    
-    // Shows the defined "maxAttempts" on screen
-    document.getElementById("maxAttempt").innerHTML = maxAttempt;
-    
-    let resetBtn = document.getElementById("resetBtn");
-    
-    function reset() {
-        
-    }
-    
-    resetBtn.addEventListener("click", reset);
-    
-    randomWord();
+// Takes random word out of array with answers
+function randomWord() {
+    answer = hangmanAnswers[Math.floor(Math.random() * hangmanAnswers.length)];
+    maxAttempt = Number(answer.length);
+    console.log(answer);
     guessBox();
-    
-    
-    
+}
+
+
+let keyboard = document.getElementById("keyboard");
+
+// Array for the keyboard
+let dataSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+
+// Takes the strings in the array and makes them to buttons
+for (let i = 0; i < dataSet.length; i++) {
+    let alphabetBtn = document.createElement("button");
+    alphabetBtn.innerHTML = dataSet[i];
+    keyboard.appendChild(alphabetBtn);
+    alphabetBtn.setAttribute("id", dataSet[i]);
+    //Add event listener and sends id with on click
+    alphabetBtn.addEventListener("click", (e) => {
+        let elementId = e.target.id;
+        displayLetter(elementId);
+    });
+}
+
+//Checks clicked element
+function displayLetter(choseLetter) {
+    console.log(choseLetter);
+
+    //Goes through array and checks for guessed letter
+    for (let i = 0; i < answer.length; i++) {
+        if (choseLetter == answer[i]) {
+            displayWord[i] = choseLetter;
+            outputWord();
+            console.log("It works!!");
+
+        } else if (choseLetter != answer[i]) {
+            let tempLetter = document.getElementById(choseLetter);
+
+            tempLetter.style.display = "none";
+        }
+
+    }
+    attempt++;
+    scoreboard.innerText = `Guess ${attempt} out of ${maxAttempt}`;
+    checkForLoss()
+    checkForWin()
+}
+function outputWord() {
+    //Sets word ------
+
+    document.getElementById("sectionP").innerHTML = displayWord.join(" ");
+    console.log("guess works");
+}
+
+//Creates the underline for guess word
+function guessBox() {
+    // displayWord = answer.split('').map(letters => (guesses.indexOf(letters) >= 0 ? letters : " _ ")).join('');
+    let answerJoin = answer.split("");
+    for (let i = 0; i < answerJoin.length; i++) {
+        displayWord.push("_");
+    }
+    outputWord();
+}
+
+//Checks if solved
+function checkForWin() {
+    //String to hold our guess
+    let tepmWord = "";
+    ///Loop through array
+    for (let i = 0; i < displayWord.length; i++) {
+        tepmWord += displayWord[i];
+    }
+
+    console.log(tepmWord);
+    if (tepmWord == answer) {
+
+        console.log("HURRAAY!!")
+        winLossText.innerText = "CHAMPION HURRAY!";
+
+    }
+
+
+}
+function checkForLoss() {
+    if (attempt == maxAttempt) {
+        winLossText.innerHTML = "YOU DIED!";
+        keyboard.style.display = "none";
+
+    }
+}
+
+
+// // Shows the defined "maxAttempts" on screen
+// document.getElementById("maxAttempt").innerHTML = maxAttempt;
+
+let resetBtn = document.getElementById("resetBtn");
+
+function reset() {
+    window.location.reload();
+
+
+
+}
+
+resetBtn.addEventListener("click", reset);
+
+randomWord();
+// guessBox();
+
+
